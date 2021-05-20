4b112e54f3ee74ef3ec2940d8b3b391f402ad8f7
daehee1159 <daehee1159@gmail.com>
2021-05-20 17:07:12 +0900

    async await

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,7 +68,7 @@ export default {
     // https://go.nuxtjs.dev/bootstrap
     'bootstrap-vue/nuxt',
     // toast ui editor
-
+    '@nuxtjs/axios'
 
   ],
 
--- pages/test.vue
+++ pages/test.vue
@@ -1,18 +1,48 @@
 <template>
   <div>
-
+    <button class="btn btn-primary" @click="testMethod">
+      TEST BUTTON
+    </button>
   </div>
 </template>
 
 <script>
-import ENV from '@/env'
-
 export default {
+  data() {
+    return {
+      data1 : [],
+      data2 : {},
+      data3 : [],
+      data4 : [],
+    }
+  },
+  methods: {
+    async testMethod() {
+      // 이 값들은 async await 를 빼고도 해보고 .then을 써서도 해보면서 테스트
+      await this.$axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(res => {
+        this.data1 = res
+        console.log("data1 결과")
+      })
+      await this.$axios.get("https://jsonplaceholder.typicode.com/posts/1")
+      .then(res => {
+        this.data2 = res
+        console.log("data2 결과")
+      })
+      await this.$axios.get("https://jsonplaceholder.typicode.com/posts/1/comments")
+      .then(res => {
+        this.data3 = res
+        console.log("data3 결과")
+      })
+      await this.$axios.get("https://jsonplaceholder.typicode.com/comments?postId=1")
+      .then(res => {
+        this.data4 = res
+        console.log("data4 결과")
+      })
+    }
+  },
   mounted() {
-    // console.log(process.env.TEST_VARIABLE)
 
-    console.log(ENV.google.googleApiKey1)
-    console.log(ENV.google.googleApiKey2)
   }
 }
 </script>
